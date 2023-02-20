b9229a0c08dbc9f84ffe65437a700b9bac064754
jesus <jesus@jesus-PROX15>
2023-02-20 17:20:04 +0100

    hecho el lsi y parte del ejercicio1 ple

--- src/ejercicio1/ejercicio1PLE.java
+++ src/ejercicio1/ejercicio1PLE.java
@@ -3,6 +3,25 @@ package ejercicio1;
 public class ejercicio1PLE {
 public static Integer tiposCafe;
 public static Integer variedadesCafe;
+public static Integer getTiposCafe() {
+	return null;
+}
+public static Integer getVariedadesCafe() {
+	//TODO
+	return null;
+}
+public static Integer getCantidad(Integer j) {
+	//TODO 
+	return null;
+}
+public static Double getBeneficio(Integer i) {
+	//TODO
+	return null;
+}
+public static Double getPorcentajeCafe(Integer i , Integer j) {
+	//TODO 
+	return null;
+}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
