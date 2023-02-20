ce16a281cad5bf4de5d272336dc5df5cbbfb6b8f
jesus <jesus@jesus-PROX15>
2023-02-20 18:06:48 +0100

    planteados los ejercicio 2 de ple

---
+++ src/ejercicio2/ejercicio2PLE.java
@@ -0,0 +1,38 @@
+package ejercicio2;
+
+public class ejercicio2PLE {
+public static Integer getCursos() {
+	//TODO
+	return null;
+}
+public static Integer  getTematicas() {
+	//TODO
+	return null;
+}
+public static Integer getCentros() {
+	//TODO
+	return null;
+}
+public static Integer getMaxCentros() {
+	//TODO
+	return null;
+}
+public static Boolean getCursoTematica(Integer i, Integer j) {
+	//TODO
+	return null;
+}
+public static Double getPrecio (Integer i) {
+	//TODO
+	return null;
+}
+public static Boolean getCursoCentro(Integer i, Integer j) {
+	//TODO
+	return null;
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
