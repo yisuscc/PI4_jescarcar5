59b1eb4927e0d7588472a69f35a3bc113802e8a5
jesus <jesus@jesus-PROX15>
2023-02-23 11:15:58 +0100

    por terminar el parseo de los datos del ej 3

--- src/_datos/DatosEjercicio3.java
+++ src/_datos/DatosEjercicio3.java
@@ -1,7 +1,11 @@
 package _datos;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import us.lsi.basictypes.Test;
+import us.lsi.common.Files2;
 
 public class DatosEjercicio3 {
 //INV1: capacidad=10; especialidad=0;
@@ -22,14 +26,40 @@ public static record Trabajo(String nombre, Integer calidad, Map<Integer,Integer
 		String nom =  aux[0].trim();
 		Integer cal = Integer.valueOf(aux[1].replace("calidad=", "").trim());
 		Map<Integer , Integer> eD = new HashMap<>();
-		String
-		for()
+		String[] aux1 = datos[1].replace("reparto=", "").trim().split(",");
+		for( String a: aux1) {
+			String[]cYv = a.replace("(", "").replace(")", "").split(":");
+			eD.put(Integer.valueOf(cYv[0].trim()),  Integer.valueOf(cYv[1].trim()));
+		}
+		return new Trabajo(nom, cal, eD);
 		
+	}
+	private static List<Investigador> investigadores;
+	private static List<Trabajo>trabajos;
+	
+	public static List<Investigador> getInvestigadores(){
+		return investigadores;
+	}
+	public static List<Trabajo> getTrabajos() {
+		return trabajos;
+	}
+	private static void test() {
+		 String fichero = "ficheros/ejercicios/Ejercicio3DatosEntrada2.txt";
+		 List<String> lsF  = Files2.linesFromFile(fichero);
+		 Integer indexTrabajos = lsF.indexOf("// TRABAJOS");
+		 List<Investigador> inv= List.copyOf(lsF.subList(1, indexTrabajos)).stream().map(s-> Investigador.create(s)).toList();
+		 List<Trabajo> trab = List.copyOf(lsF.subList(indexTrabajos+1, lsF.size())).stream().map(s-> Trabajo.create(s)).toList();
+		 trabajos = trab;
+		 investigadores = inv;
+		 System.out.println("Estos son los trabajos"+ getTrabajos());
+		 System.out.println("Estos son los inv" + getInvestigadores());
+		 
 	}
 	
 }
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 test();
 
 	}
 
