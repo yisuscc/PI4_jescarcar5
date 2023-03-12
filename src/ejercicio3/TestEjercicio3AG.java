e7f800ad673b99b66352386d1efbf431e05a8834
jesus <jesus@jesus-PROX15>
2023-03-12 14:12:55 +0100

    mejorada la presentación del test ejecrcicio 3 ag

--- src/ejercicio3/TestEjercicio3AG.java
+++ src/ejercicio3/TestEjercicio3AG.java
@@ -11,19 +11,14 @@ import us.lsi.ag.agchromosomes.AlgoritmoAG;
 import us.lsi.ag.agstopping.StoppingConditionFactory;
 
 public class TestEjercicio3AG {
-	// lo voy a hacer en test inndividuales, porque si no, me da problemas en la visualización
+	// lo voy a hacer en test inndividuales, porque si no, me da problemas en la
+	// visualización
 	public static void test1() {
-		Locale.setDefault(new Locale("en", "US"));
-		AlgoritmoAG.ELITISM_RATE  = 0.30;
-		AlgoritmoAG.CROSSOVER_RATE = 0.80;
-		AlgoritmoAG.MUTATION_RATE = 0.7;
-		AlgoritmoAG.POPULATION_SIZE = 500;
-		StoppingConditionFactory.NUM_GENERATIONS = 1000;
-		StoppingConditionFactory.stoppingConditionType = StoppingConditionFactory.StoppingConditionType.GenerationCount;
+
+		// DatosEjercicio3.test(fichero);
 		String fichero = "ficheros/ejercicios/Ejercicio3DatosEntrada1.txt";
-		//DatosEjercicio3.test(fichero);
 		Ejercicio3AG p = new Ejercicio3AG(fichero);
-		AlgoritmoAG<List<Integer>,SolucionEjercicio3> ap = AlgoritmoAG.of(p);
+		AlgoritmoAG<List<Integer>, SolucionEjercicio3> ap = AlgoritmoAG.of(p);
 		ap.ejecuta();
 		System.out.println("================================");
 		System.out.println(ap.bestSolution());
@@ -31,23 +26,28 @@ public class TestEjercicio3AG {
 		System.out.println(ap.getBestChromosome().fitness());
 		System.out.println("================================");
 	}
+
 	public static void test2() {
-Locale.setDefault(new Locale("en", "US"));
-		
-		AlgoritmoAG.ELITISM_RATE  = 0.30;
-		AlgoritmoAG.CROSSOVER_RATE = 0.80;
-		AlgoritmoAG.MUTATION_RATE = 0.7;
-		AlgoritmoAG.POPULATION_SIZE = 500;
-		
-		StoppingConditionFactory.NUM_GENERATIONS = 1000;
-		StoppingConditionFactory.stoppingConditionType = StoppingConditionFactory.StoppingConditionType.GenerationCount;
-		
 		String fichero = "ficheros/ejercicios/Ejercicio3DatosEntrada2.txt";
-		
+
 		Ejercicio3AG p = new Ejercicio3AG(fichero);
-		AlgoritmoAG<List<Integer>,SolucionEjercicio3> ap = AlgoritmoAG.of(p);
+		AlgoritmoAG<List<Integer>, SolucionEjercicio3> ap = AlgoritmoAG.of(p);
 		ap.ejecuta();
-		
+
+		System.out.println("================================");
+		System.out.println(ap.bestSolution());
+		System.out.println("Fitness: ");
+		System.out.println(ap.getBestChromosome().fitness());
+		System.out.println("================================");
+	}
+
+	public static void test3() {
+
+		String fichero = "ficheros/ejercicios/Ejercicio3DatosEntrada3.txt";
+
+		Ejercicio3AG p = new Ejercicio3AG(fichero);
+		AlgoritmoAG<List<Integer>, SolucionEjercicio3> ap = AlgoritmoAG.of(p);
+		ap.ejecuta();
 
 		System.out.println("================================");
 		System.out.println(ap.bestSolution());
@@ -57,10 +57,19 @@ Locale.setDefault(new Locale("en", "US"));
 	}
 
 	public static void main(String[] args) {
-//test1();
 
-test2();
-		
+		// Importante ejecutar cada uno de forma Individual
+		// si no da problemas en la presentación
+		Locale.setDefault(new Locale("en", "US"));
+		AlgoritmoAG.ELITISM_RATE = 0.30;
+		AlgoritmoAG.CROSSOVER_RATE = 0.80;
+		AlgoritmoAG.MUTATION_RATE = 0.7;
+		AlgoritmoAG.POPULATION_SIZE = 500;
+		StoppingConditionFactory.NUM_GENERATIONS = 1000;
+		StoppingConditionFactory.stoppingConditionType = StoppingConditionFactory.StoppingConditionType.GenerationCount;
+		test1();
+		test2();
+		test3();
 
 	}
 
